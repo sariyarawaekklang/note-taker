3f0516bc7bb80ac98c39c352902cbf1d109973a7
sariyarawaekklang <sariyarawaekklang@gmail.com>
2022-01-23 20:13:41 -0600

    started apiRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,3 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const router = require('express').Router();
+
+router.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../db/db.json'));
+});
+
+router.post('/api/notes', (req, res) => {
+    
+})
 
 module.exports = router;
